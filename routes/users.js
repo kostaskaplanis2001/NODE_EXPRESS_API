9df5160a291a0b5d57cbef915b6be914dd50828e
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-20 22:32:04 +0300

    Added update request and created controllers folder for the users

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,26 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
 let users = [];
 
 // All routes in here are starting with /users
-router.get('/', (req, res) => {
-  console.log(users);
+router.get('/', getUsers);
 
-  res.send(users);
-});
-
-router.post('/', (req, res) => {
-  const user = req.body;
-
-  const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-
-  const userWithId = { ...user, id: userId };
-
-  users.push(userWithId);
-
-  res.send(`User with the name ${user.firstname} added to the database`);
-});
+router.post('/', createUser);
 
 // /users/2 => req.params { id: 2 }
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.get('/:id', getUser);
 
-  const foundUser = users.find((user) => user.id === id);
+router.delete('/:id', deleteUser);
 
-  res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  // id to delete 123
-
-  // John 123
-  // Jane 321
-
-  users = users.filter((user) => user.id !== id);
-
-  res.send(`User with the id ${id} deleted from the database`);
-});
+router.patch('/:id', updateUser);
 
 export default router;
