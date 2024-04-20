300368c0ed5353890d290b16d123cb7a9d6bd654
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-20 21:52:21 +0300

    Added uuid and created /users/id request

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstname: 'Kostantinos',
-    lastname: 'Kaplanis',
-    age: 25,
-  },
-  {
-    firstname: 'Jane',
-    lastname: 'Doe',
-    age: 23,
-  },
-];
+const users = [];
 
 // All routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -24,9 +14,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const user = req.body;
-  users.push(user);
+
+  const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+  const userWithId = { ...user, id: userId };
+
+  users.push(userWithId);
 
   res.send(`User with the name ${user.firstname} added to the database`);
+});
+
+// /users/2 => req.params { id: 2 }
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
 });
 
 export default router;
