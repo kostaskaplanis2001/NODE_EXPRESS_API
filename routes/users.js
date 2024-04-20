d428d496f8f6255072cfd2683c2c84ff0f71874d
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2024-04-20 21:39:51 +0300

    Created a Mock database and created a post request for /users

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,31 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstname: 'Kostantinos',
+    lastname: 'Kaplanis',
+    age: 25,
+  },
+  {
+    firstname: 'Jane',
+    lastname: 'Doe',
+    age: 23,
+  },
+];
+
 // All routes in here are starting with /users
 router.get('/', (req, res) => {
-  res.send('Hello');
+  console.log(users);
+
+  res.send(users);
+});
+
+router.post('/', (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(`User with the name ${user.firstname} added to the database`);
 });
 
 export default router;
